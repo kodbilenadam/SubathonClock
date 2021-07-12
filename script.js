cff1d25ac62186664ebb070af32d4e28fdac1e57
Çağatay Karahan <kodbilenadam@gmail.com>
2021-07-12 03:58:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,11 @@ window.onload = function() {
         secondsInAHour = 60 * 60 * 1000;
       
     days = Math.floor(timeDifference / (secondsInADay) * 1);
-    years = Math.floor(days / 365);
-    if (years > 1){ days = days - (years * 365) }
     hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
   
     var idEl = document.getElementById(id);
-    idEl.getElementsByClassName('years')[0].innerHTML = years;
     idEl.getElementsByClassName('days')[0].innerHTML = days;
     idEl.getElementsByClassName('hours')[0].innerHTML = hours;
     idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
